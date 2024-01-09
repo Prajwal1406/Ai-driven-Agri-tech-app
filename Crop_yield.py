dc9ce163021ea722aaf9c50b6b2ba2c6f59a9989
Prajwal J P <119681594+Prajwal1406@users.noreply.github.com>
2024-01-10 00:55:14 +0530

    Update Crop_yield.py

--- Crop_yield.py
+++ Crop_yield.py
@@ -402,10 +402,7 @@ def Crop_yield():
         state = 'Karnataka'
         try:
                 area = col2.number_input("Enter area (e.g., in ha)", min_value=1.0, max_value=10000000.0, value=6637.0, step=1.0, format="%f", help="Enter the area in Hacter")
-                minallowed = area * 1
-                # Set a maximum limit for minallowed to avoid data leak error
-                minallowed_limit = 1.0
-                minallowed = max(minallowed, minallowed_limit)
+                minallowed = area * 0.03
                 maxallowed = area * 30
                 production = col1.number_input('Enter production (e.g., in kg)', value=6000, min_value=minallowed, max_value=maxallowed, step=10.0)
                 annual_rainfall = col2.number_input('Enter annual rainfall (e.g., in mm)',value=2051.4,min_value=200.0,max_value=2500.0,step=100.0)
